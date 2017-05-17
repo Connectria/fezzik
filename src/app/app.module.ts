612897f0e1090572f77b3d2fc2f4c417b300f54e
Jake Helbig <jhelbig@connectria.com>
2017-05-17 15:57:02 -0500

    completed tasks, good luck!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,32 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MessagesService } from './messages.service';
+import { PostsService } from './posts.service';
+
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    JumbotronComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    MessagesService,
+    PostsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
